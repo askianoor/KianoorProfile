4b2e18125036b87583a59b69eebcf6f217386126
Ali Kianoor <askianoor@gmail.com>
2020-04-09 00:00:00 +0430

    Add Contact API and model

---
+++ Askianoor/Controller/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Askianoor.Models.Main;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using Askianoor.Models;
+using Microsoft.Extensions.Options;
+
+namespace Askianoor.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApplicationSettings _appSettings;
+
+        public ContactController( IOptions<ApplicationSettings> appSettings)
+        { 
+            if (appSettings != null)
+            {
+                _appSettings = appSettings.Value;
+            }
+        }
+
+        [HttpPost]
+        public bool PostContact(Contact contact)
+        {
+            if (contact == null)
+            {
+                return false;
+            }
+
+            MailAddress to = new MailAddress(_appSettings.ContactEmail);
+            MailAddress from = new MailAddress(contact.Email);
+
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = contact.Subject;
+            message.Body = contact.Message;
+
+            SmtpClient client = new SmtpClient("smtp.server.address", 2525)
+            {
+                Credentials = new NetworkCredential("smtp_username", "smtp_password"),
+                EnableSsl = true
+            };
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return CreatedAtAction("GetSkill", new { id = skill.SkillId }, skill);
+        }
+    }
+}
--- Askianoor/Models/Main/ApplicationSettings.cs
+++ Askianoor/Models/Main/ApplicationSettings.cs
@@ -12,5 +12,17 @@ namespace Askianoor.Models
         private string ClientURL { get; set; }
 
         public string AdminRoleName { get; set; }
+
+        public string ReCaptchaSecretKey { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public string SmtpServer { get; set; }
+
+        public string SmtpPort { get; set; }
+
+        public string SmtpUser { get; set; }
+
+        public string SmtpPassword { get; set; }
     }
 }
